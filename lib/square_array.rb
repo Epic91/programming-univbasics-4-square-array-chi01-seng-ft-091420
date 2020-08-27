2fc774e7dfaf0edf8d9b755e7cedfa1a9d460854
Evelyn Hernandez <hernandeze791@gmail.com>
2020-08-27 19:41:52 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,12 +1,3 @@
-def square_array(array)
-  new_array = []
-  array.each { |num|
-    s = Math.sqrt(num)
-    if (s - s.floor).zero?
-      new_array << s.to_i
-    else
-      new_array << num * num
-    end
-  }
-  puts new_array
+def square_array
+  puts
 end
