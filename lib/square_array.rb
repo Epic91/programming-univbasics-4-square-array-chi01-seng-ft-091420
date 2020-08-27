dd08a6665821fa8fc7f26987d4a8f60649188d17
Evelyn Hernandez <hernandeze791@gmail.com>
2020-08-27 20:57:55 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,4 +1,10 @@
 def square_array
-  numbers = [1,2,3]
-  square_array(numbers)
+  [1,2,3].each do |num|
+    num ** 2
+  end
+
+  new_array = []
+  [1,4,9].each do |num|
+    new_array << num ** 2
+  end
 end
