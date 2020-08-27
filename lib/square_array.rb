0d626ae845efb3067b2b1b9674de6c14b3106e75
Evelyn Hernandez <hernandeze791@gmail.com>
2020-08-27 19:27:57 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,6 +1,12 @@
 def square_array(array)
+  class integer
+    def square?
+      Math.sqrt(self).to_i ** 2 ==self
+    end
+  end
+  
   array.map do |num|
-    if (num.sqrt?)
+    if (num.square?)
       num
     else
       num ** 2
